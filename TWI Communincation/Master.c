eb924f9dfe900dcd351861c4d5b529189ace153e
navneet-13 <87007888+navneet-13@users.noreply.github.com>
2021-07-21 00:05:22 +0530

    Update and rename main.c to Master.c

--- TWI Communincation/Master.c
+++ TWI Communincation/Master.c
@@ -13,11 +13,10 @@ int main(void){
 	int pressed=0;
 	
 	
-	
-    while (1) {
+	while (1) {
 		
 				
-		if(PINB == 0b00000000){                         //check if switched is pressed
+		if(PINB == 0b00000000){                         		//check if switched is pressed
 			if(pressed == 0){                     
 				pressed=1;
 				PORTA ^= (1<<PINA0);
@@ -25,10 +24,10 @@ int main(void){
 				
 				TWCR = 1<<TWINT | 1<<TWEN | 1<<TWSTA;		/*START*/
 				while(!(TWCR & 1<<TWINT)){
-					;	}			                 /* Wait for TWINT flag to be raised*/
+					;	}			 	/* Wait for TWINT flag to be raised*/
 				while(!(TWSR == 0b00001000)){
-					;  }                              /*Check Status register for start condition*/
-				TWDR = 0b10010010;						/*Write address of slave and write command*/ 
+					;  }                              	/*Check Status register for start condition*/
+				TWDR = 0b10010010;				/*Write address of slave and write command*/ 
 				TWCR = 1<<TWINT | 1<<TWEN;   			/*Start sending address by clearing TWINT Flag*/ 
 					    
 					    
@@ -40,7 +39,7 @@ int main(void){
 				TWCR = 1<<TWINT | 1<<TWEN;			/*Clear TWINT Flag and data is sent*/
 						 
 				while(!(TWCR & 1<<TWINT)){
-					;        }                      		/*wait for TWINT flag to be set*/
+					;        }                      	/*wait for TWINT flag to be set*/
 				while(!(TWSR == 0x28)){
 					;         } 
 				TWCR = 1<<TWINT | 1<<TWEN | 1<<TWSTO;     /*Stop command sent*/
@@ -56,12 +55,12 @@ int main(void){
 				TWCR = 1<<TWINT | 1<<TWEN;
 				while(!(TWCR & 1<<TWINT))                  //Wait for Twint Flag to be set
 					;
-				while(!(TWSR == 0x40))					   //Check Status register
+				while(!(TWSR == 0x40))		           //Check Status register
 					;        		
-			    TWCR = 1<<TWINT | 1<<TWEA | 1<<TWEN;
+			    	TWCR = 1<<TWINT | 1<<TWEA | 1<<TWEN;
 				while(!(TWCR & 1<<TWINT))
 					;
-				PORTC = TWDR ^ 0xFF;						//read the received data
+				PORTC = TWDR ^ 0xFF;			    //read the received data
 				while(!(TWSR == 0x50 || TWSR == 0x58))
 					;
 				
